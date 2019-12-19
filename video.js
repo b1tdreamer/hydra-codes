28562138a1b7e8e80add195856e767178e48d9bf
b1t <b1tdreamer@users.noreply.github.com>
2019-12-19 12:03:39 -0500

    Update video.js

--- video.js
+++ video.js
@@ -6,16 +6,22 @@ vid.loop = true
 vid.src = 'E:/No sync/GimbriSong/Proxies/DJI_0775_Proxy.mp4'
 //atom.project.getPaths()[0]+'/assets/codeground.webm'
 
+s0.initCam()
+src(s0).out(o0)
+
 // use video within hydra
-s0.init({src: vid})
+s1.init({src: vid})
 src(s0)
-  .scale(1.8,1)
+  .scale(1,1)
+  .modulate(noise(0.9))
 //  .rotate(0, 0.2)
 //  .repeat(5, 3, 0.5)
 //  .saturate(3.0)
   .color(1.0, 0.7, -1)
   .scrollX(0, -0.1)
 //  .diff(osc(0.2, 0.1, 0.2))
-  .kaleid(5)
+  .kaleid(3)
   .blend(s0,0.75)
-  .out()
+  .out(o1)
+
+render(o1)
